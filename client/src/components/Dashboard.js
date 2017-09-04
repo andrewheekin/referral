a253b981cfca75d0b8fab9ce0b40eedde814aa41
Andrew Heekin <andrewheekin@gmail.com>
2017-09-03 20:57:31 -0400

    if user wasnt referred

--- client/src/components/Dashboard.js
+++ client/src/components/Dashboard.js
@@ -6,9 +6,10 @@ const Dashboard = ({ data }) => (
   <div className="container">
     <h2>Welcome, {data.name}!</h2>
     <p>Your email is {data.email}</p>
-    <p>You were referred by {data.referrer}</p>
+    { data.referrer ? (<p>You were referred by {data.referrer}</p>) :
+                           (<p>You weren't referred by another user.</p>)}    
     { data.referrals_count > 0 ? (<p>You have {data.referrals_count} registered referrals: {data.referrals}</p>) :
-                           (<p>You have no registered referrals.</p>)}    
+                           (<p>You have no registered referrals.</p>)}
 
   </div>
 );
